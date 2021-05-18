155608d39c7fae34cd034415b2cdfe45ba5943ce
znareak <libardojesusrengifo129@gmail.com>
2021-05-18 12:09:54 -0400

    change url api

--- client/src/Config/config.js
+++ client/src/Config/config.js
@@ -1,6 +1,6 @@
 import { getToken } from "../Helpers/token";
 
-const API_URL = "http://localhost:4000/api";
+const API_URL = "https://portfolio-api-phi.vercel.app/api";
 const LOGIN = `${API_URL}/login`;
 const USER_INFO = `${API_URL}/user?token=${getToken()}`;
 const PROJECTS= `${API_URL}/project`;
