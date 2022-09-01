13453d5a07c6754096514244c8928987f32d2dc7
znareak <libardojesusrengifo129@gmail.com>
2022-09-01 18:06:57 -0400

    dev: caching languages

--- src/config/i18next.js
+++ src/config/i18next.js
@@ -1,14 +1,26 @@
 import i18next from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import BackendI18Next from "i18next-http-backend";
+import ChainedBackend from "i18next-chained-backend";
+import HttpBackend from "i18next-http-backend";
+import LocalStorageBackend from "i18next-localstorage-backend";
 import { initReactI18next } from "react-i18next";
 
 i18next
   .use(initReactI18next)
-  .use(BackendI18Next)
+  .use(ChainedBackend)
   .use(LanguageDetector)
 
   .init({
-    lng: "es",
+    backend: {
+      backends: [LocalStorageBackend, HttpBackend],
+      backendOptions: [
+        {
+          expirationTime: 7 * 24 * 60 * 60 * 1000, // 7 days
+        },
+        {
+          loadPath: "../../public/locales/{{lng}}/{{ns}}.json",
+        },
+      ],
+    },
     fallbackLng: "en",
   });
