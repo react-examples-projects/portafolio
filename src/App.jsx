fbe3675a2b93ad01166fdd4da8c89d8e0328e263
znareak <libardojesusrengifo129@gmail.com>
2023-03-20 14:24:23 +0100

    dev: gradient image & new projects!

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import Contact from "./components/Contact";
 import Skills from "./components/Skills";
 import Footer from "./components/Footer";
 
+import gradient1 from "./resources/gradient-1.png";
+
 function App() {
   return (
     <div className="wrapper fadeIn">
       <Navbar />
       <main className="container">
+        <img src={gradient1} className="gradient1" alt="gradient figure 1" />
         <AboutMe />
         <Projects />
         <Skills />
