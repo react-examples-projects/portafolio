fd7755fda77bab3e90449c10c2fae031652e3b46
znareak <libardojesusrengifo129@gmail.com>
2024-05-11 20:05:22 +0200

    dev: add new cursor

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,10 +5,32 @@ export default function Project({ _id, technologies, title, description, github,
   return (
     <article className={styles.projectContainer} key={_id} {...props}>
       <div className={styles.project}>
-        <Link href={link} target="_blank" rel="noreferrer noopener">
-          <h3>{title}</h3>
-        </Link>
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+          <Link href={link} target="_blank" rel="noreferrer noopener">
+            <h3>{title}</h3>
+          </Link>
+          <div style={{ display: "flex", alignItems: "center", fontSize: "14px" }}>
+            {github && (
+              <Link href={github} target="_blank" rel="noreferrer noopener" color>
+                <small>Github</small>
+              </Link>
+            )}
+            {link && (
+              <Link
+                href={link}
+                target="_blank"
+                rel="noreferrer noopener"
+                style={{ marginLeft: github ? "1rem" : "0" }}
+                color
+              >
+                <small>Demo</small>
+              </Link>
+            )}
+          </div>
+        </div>
+
         <p className={styles.description}>{description}</p>
+
         <div className={styles.technologies}>
           {technologies.map((tech) => (
             <Tag scale={0.6} key={tech}>
@@ -17,7 +39,7 @@ export default function Project({ _id, technologies, title, description, github,
           ))}
         </div>
 
-        <div style={{ marginTop: "0.5rem" }}>
+        {/* <div style={{ marginTop: "0.5rem" }}>
           {github && (
             <Link href={github} target="_blank" rel="noreferrer noopener" color>
               <small>Github</small>
@@ -34,7 +56,7 @@ export default function Project({ _id, technologies, title, description, github,
               <small>Demo</small>
             </Link>
           )}
-        </div>
+        </div> */}
       </div>
     </article>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@ import styles from "../styles/projects.module.scss";
 import useProjects from "../hooks/useProjects";
 import Project from "./Project";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
-import { Button, Select } from "@geist-ui/core";
+import { Button, Select, Grid } from "@geist-ui/core";
 import { useTranslation } from "react-i18next";
 import { useEffect } from "react";
 import usePagination from "../hooks/usePagination";
@@ -47,6 +47,7 @@ export default function Projects() {
           </Select>
         </div>
       </div>
+
       {projects.length > ITEMS_PER_PAGE && (
         <div
           style={{
@@ -57,23 +58,10 @@ export default function Projects() {
           }}
         >
           <Button
-            onClick={nextPage}
-            className={styles2.button}
-            type="secondary"
-            style={{ marginRight: "5px", lineHeight: 3 }}
-            scale={0.5}
-            aria-label={t("next")}
-            disabled={currentPage === pages}
-            ghost
-            auto
-          >
-            &rarr;
-          </Button>
-          <Button
             onClick={previousPage}
             className={styles2.button}
             type="secondary"
-            style={{ lineHeight: 3 }}
+            style={{ marginRight: "5px", lineHeight: 3 }}
             scale={0.5}
             aria-label={t("prev")}
             disabled={currentPage === 1}
@@ -82,15 +70,37 @@ export default function Projects() {
           >
             &larr;
           </Button>
+          <Button
+            onClick={nextPage}
+            className={styles2.button}
+            type="secondary"
+            style={{ lineHeight: 3 }}
+            scale={0.5}
+            aria-label={t("next")}
+            disabled={currentPage === pages}
+            ghost
+            auto
+          >
+            &rarr;
+          </Button>
         </div>
       )}
-      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 2 }}>
+
+      <Grid.Container gap={1.5}>
+        {data.map((props) => (
+          <Grid xs={24} sm={24} md={12} lg={12} xl={12} key={props._id}>
+            <Project {...props} />
+          </Grid>
+        ))}
+      </Grid.Container>
+
+      {/* <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 2 }}>
         <Masonry gutter="12px" columnsCount={2} className={styles.column}>
           {data.map((props) => (
             <Project {...props} key={props._id} />
           ))}
         </Masonry>
-      </ResponsiveMasonry>
+      </ResponsiveMasonry> */}
     </section>
   );
 }
